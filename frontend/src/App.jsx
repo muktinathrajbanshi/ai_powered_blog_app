1af86c3d788d134d4c8f3d831eb1a5a41a0c82dc
unknown <muktinathrajbanshi786@gmail.com>
2026-06-15 19:05:50 +0545

    aja lai yati la manai sancho chhaina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </div>
   );
