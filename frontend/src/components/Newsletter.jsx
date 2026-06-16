dffac1921b0bdf79c1170d537ab71f229ab18282
unknown <muktinathrajbanshi786@gmail.com>
2026-06-16 21:51:13 +0545

    hai garmi

---
+++ frontend/src/components/Newsletter.jsx
@@ -0,0 +1,10 @@
+const Newsletter = () => {
+  return (
+    <div>
+      <h1>Never Miss a Blog!</h1>
+      <p>Subscribe to get the latest blog, new tech, and exclusive news.</p>
+    </div>
+  );
+};
+
+export default Newsletter;
