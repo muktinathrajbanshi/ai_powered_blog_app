9abb664b9e989918bbe2386b429453e6cdea3472
unknown <muktinathrajbanshi786@gmail.com>
2026-06-17 14:03:23 +0545

    hait aago man

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { blog_data } from "../assets/assets";
+import { assets, blog_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
+import Moment from "moment";
 
 const Blog = () => {
   const { id } = useParams();
@@ -18,9 +19,21 @@ const Blog = () => {
   }, []);
 
   return data ? (
-    <div>
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        alt=""
+        className="absolute -top-50 -z-1 opacity-50"
+      />
+
       <Navbar />
-      <div></div>
+
+      <div>
+        <p>Published on {Moment(data.createdAt).format("MMMM Do YYYY")}</p>
+        <h1>{data.title}</h1>
+        <h2>{data.subTitle}</h2>
+        <p>Muktinath Rajbanshi</p>
+      </div>
 
       <div></div>
     </div>
