8265a76b898e278debeaac8b7868ac174328ed93
unknown <muktinathrajbanshi786@gmail.com>
2026-06-17 13:50:08 +0545

    hai garmi

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { blog_data } from "../assets/assets";
+import Navbar from "../components/Navbar";
+
 const Blog = () => {
-  return (
+  const { id } = useParams();
+
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+
+  return data ? (
     <div>
-      <h1>Blog</h1>
+      <Navbar />
+      <div></div>
+
+      <div></div>
     </div>
+  ) : (
+    <div>Loading...</div>
   );
 };
 
