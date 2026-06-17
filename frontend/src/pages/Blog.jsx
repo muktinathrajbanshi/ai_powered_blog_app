7aa7ebb8fa583f7f7d8cfbf90f075b5c1cf4994e
unknown <muktinathrajbanshi786@gmail.com>
2026-06-17 14:12:02 +0545

    wow k saro diusai nidra lageko haha

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -28,11 +28,23 @@ const Blog = () => {
 
       <Navbar />
 
-      <div>
-        <p>Published on {Moment(data.createdAt).format("MMMM Do YYYY")}</p>
-        <h1>{data.title}</h1>
-        <h2>{data.subTitle}</h2>
-        <p>Muktinath Rajbanshi</p>
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-primary py-4 font-medium">
+          Published on {Moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <h1
+          className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto
+        text-gray-800"
+        >
+          {data.title}
+        </h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+        <p
+          className="inline-block py-1 px-4 rounded-full mb-6 border text-sm
+        border-primary/35 bg-primary/5 font-medium text-primary"
+        >
+          Muktinath Rajbanshi
+        </p>
       </div>
 
       <div></div>
