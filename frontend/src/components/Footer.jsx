24ac7bf4117401cc41ab9c2b0bd28285bc8a028b
unknown <muktinathrajbanshi786@gmail.com>
2026-06-17 11:14:37 +0545

    i wish her everything will be ok

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div></div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2026 © QuickBlog Muktinath Tech - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
