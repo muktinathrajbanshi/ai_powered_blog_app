6231b17703e30cff0415b892388d4af79ba96023
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-19 16:24:35 +0545

    aja lai yati la

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { assets, dashboard_data } from "../../assets/assets";
+
 const Dashboard = () => {
-  return <div></div>;
+  const [dashboardData, setDashboardData] = useState({
+    blogs: 0,
+    comments: 0,
+    drafts: 0,
+    recentBlogs: [],
+  });
+
+  const fetchDashboard = async () => {
+    setDashboardData(dashboard_data);
+  };
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+
+  return (
+    <div className="flex-1 p-4 md:p-10 bg-blue-50/50">
+      <div className="flex flex-wrap gap-4">
+        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
+          <img src={assets.dashboard_icon_1} alt="" />
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.blogs}
+            </p>
+            <p className="text-gray-400 font-light">Blogs</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
