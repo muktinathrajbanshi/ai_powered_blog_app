54e154c4b0e933f4e7c062a59d7e8cfe3c1f6139
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-19 15:17:51 +0545

    admin dashboard is looking good

---
+++ frontend/src/pages/admin/Layout.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { assets } from "../../assets/assets";
+
+const Layout = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="logo"
+          className="w-32 sm:w-40 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+        <button
+          onClick={logout}
+          className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ frontend/src/pages/admin/ListBlog.jsx
@@ -0,0 +1,5 @@
+const ListBlog = () => {
+  return <div></div>;
+};
+
+export default ListBlog;
