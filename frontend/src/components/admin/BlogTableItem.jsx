cccab44a6af405373833ed12a881a923dabf8ce3
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-20 17:37:30 +0545

    wow muktinath tune ye kiya kardiya mamu

--- frontend/src/components/admin/BlogTableItem.jsx
+++ frontend/src/components/admin/BlogTableItem.jsx
@@ -1,3 +1,5 @@
+import { assets } from "../../assets/assets";
+
 const BlogTableItem = ({ blog, fetchBlogs, index }) => {
   const { title, createdAt } = blog;
   const BlogDate = new Date(createdAt);
@@ -13,6 +15,16 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
         >
           {blog.isPublished ? "Published" : "Unpublished"}
         </p>
+      </td>
+      <td className="px-2 py-4 flex text-xs gap-3">
+        <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+          {blog.isPublished ? "Unpublish" : "Publish"}
+        </button>
+        <img
+          src={assets.cross_icon}
+          className="w-8 hover:scale-110 transition-all cursor-pointer"
+          alt="img"
+        />
       </td>
     </tr>
   );
