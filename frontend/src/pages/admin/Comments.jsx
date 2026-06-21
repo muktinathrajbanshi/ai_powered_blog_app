b144066138f111ced18847231088644ad112826b
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-21 20:24:38 +0545

    la vok lago ta man

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -19,11 +19,29 @@ const Comments = () => {
         <h1>Comments</h1>
         <div className="flex gap-4">
           <button
-            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs`}
+            onClick={() => setFilter("Approved")}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs 
+            ${filter === "Approved" ? "text-primary" : "text-gray-700"}`}
           >
             Approved
           </button>
+          <button
+            onClick={() => setFilter("Not Approved")}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs 
+            ${filter === "Not Approved" ? "text-primary" : "text-gray-700"}`}
+          >
+            Not Approved
+          </button>
         </div>
+      </div>
+      <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-700 text-left uppercase">
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </div>
   );
