417c8e1d79f6d009f3de016c62033e884ff1b0fc
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-22 19:52:49 +0545

    aja dhilo vayo yar

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,9 +1,11 @@
 
 const AddBlog = () => {
   return (
-    <div>
+    <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
       
-    </div>
+      </div>
+    </form>
   )
 }
 
