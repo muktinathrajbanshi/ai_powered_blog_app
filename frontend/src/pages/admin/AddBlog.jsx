4a6cce13d57cd285a505b4f758a312ae7b38242f
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-22 20:01:12 +0545

    la hoki kiya ho

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,12 +1,19 @@
-
 const AddBlog = () => {
   return (
     <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
       <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
-      
+        <p>Upload thumbnail</p>
+        <label>
+          <img
+            src={assets.upload_area}
+            alt=""
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input type="file" id="image" hidden required />
+        </label>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default AddBlog
+export default AddBlog;
