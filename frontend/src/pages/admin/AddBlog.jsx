6f1de148c25ac127dba087eee84927f7eca5535d
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-22 20:16:25 +0545

    laga gai oi laga gai

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -40,6 +40,18 @@ const AddBlog = () => {
           placeholder="Type here"
           required
           className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <p className="mt-4">Sub title</p>
+        <input
+          type="text"
+          placeholder="Type here"
+          required
+          className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+          onChange={(e) => setSubTitle(e.target.value)}
+          value={subTitle}
         />
       </div>
     </form>
