9a3eb7a52e187830a371421f27d2f7d70ede91b8
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-22 20:04:26 +0545

    chamaila are vidu gopal aiya

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+
 const AddBlog = () => {
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState(false);
+
   return (
     <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
       <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
