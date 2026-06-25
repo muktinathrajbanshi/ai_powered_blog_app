083cc8622b3647e6f893f0755a382b2d23d3c031
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-25 22:04:20 +0545

    haina aja kata ho

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -67,7 +67,7 @@ export const getAllBlogs = async (req, res) => {
 
 export const getBlogById = async (req, res) => {
   try {
-    const { blogId } = req.parse;
+    const { blogId } = req.params;
     const blog = await Blog.findById(blogId);
     if (!blog) {
       return res.json({ success: false, message: "Blog not found" });
@@ -75,7 +75,7 @@ export const getBlogById = async (req, res) => {
 
     res.json({
       success: true,
-      blogs,
+      blog,
     });
   } catch (error) {
     res.json({
