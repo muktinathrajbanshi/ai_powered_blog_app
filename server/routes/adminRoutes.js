46087f71924f17931e75e01e52e458467e244151
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-26 22:11:31 +0545

    hey vagban kina yesto

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -5,6 +5,7 @@ import {
   deleteCommentById,
   getAllBlogsAdmin,
   getAllComments,
+  getDashboard,
 } from "../controllers/adminController.js";
 import auth from "../middleware/auth.js";
 
@@ -15,5 +16,6 @@ adminRouter.get("/comments", auth, getAllComments);
 adminRouter.get("/blogs", auth, getAllBlogsAdmin);
 adminRouter.post("/delete-comment", auth, deleteCommentById);
 adminRouter.post("/approve-comment", auth, approveCommentById);
+adminRouter.get("/dashboard", auth, getDashboard);
 
 export default adminRouter;
