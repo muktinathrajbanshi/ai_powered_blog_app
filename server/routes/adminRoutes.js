b9f62f114f7c89394009f182b19b2759d62ae9ec
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-26 21:58:10 +0545

    hey vagban kina yesto

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { adminLogin } from "../controllers/adminController.js";
+import {
+  adminLogin,
+  deleteCommentById,
+  getAllBlogsAdmin,
+  getAllComments,
+} from "../controllers/adminController.js";
+import auth from "../middleware/auth.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.post("/delete-comment", auth, deleteCommentById);
 
 export default adminRouter;
