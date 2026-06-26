83b675794bf530d72dbbafff4dee755b1e471ca2
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-26 22:44:56 +0545

    Hey vagban kuch to bata

--- frontend/context/AppContext.jsx
+++ frontend/context/AppContext.jsx
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const AppContext = createContext();
 
@@ -6,4 +9,8 @@ export const AppProvider = ({ children }) => {
   const value = {};
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
 };
