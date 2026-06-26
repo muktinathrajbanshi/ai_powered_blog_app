e26641c3289b1e1cbcd2141ef89cf04928291bb4
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-06-26 22:16:30 +0545

    frontend and backend completed now it's time for integrating both as a single one

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -10,6 +10,6 @@ const commentSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-const Comment = mongoose.model("Comment", blogSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
 export default Comment;
